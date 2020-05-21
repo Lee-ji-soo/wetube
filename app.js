53230d8273c7af2c01302d4a07c831a4e7f92e51
Lee-ji-soo <nbsp3@naver.com>
2020-05-22 00:31:14 +0900

    learn MVC, learn import and export

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
 import globalRouter from "./routers/globalRouter.js";
+import routes from "./routes.js";
 
 const app = express()
 
@@ -15,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("tiny"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 
 export default app;
